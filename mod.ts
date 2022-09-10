d0a8a2f91444b0f49bfcfaded0412420ad6564d4
silva4dev <web4dev.lucas@outlook.com>
2022-09-10 17:01:44 -0300

    inicial

---
+++ mod.ts
@@ -0,0 +1,49 @@
+import { join } from "https://deno.land/std@0.155.0/path/mod.ts";
+import { parse } from "https://deno.land/std@0.155.0/encoding/csv.ts";
+import { pick } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+
+interface Planet {
+  [key: string]: string;
+}
+
+async function loadPlanetData() {
+  const path = join(".", "kepler_exoplanets_nasa.csv");
+  const file = await Deno.open(path);
+  const result = await parse(await Deno.readTextFile(path), {
+    skipFirstRow: true,
+    comment: "#",
+  });
+
+  Deno.close(file.rid);
+  const planets = (result as Array<Planet>).filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    return (
+      planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 &&
+      planetaryRadius < 1.5 &&
+      stellarRadius > 0.99 &&
+      stellarRadius < 1.01 &&
+      stellarMass > 0.78 &&
+      stellarMass < 1.04
+    );
+  });
+
+  return planets.map((planet) => {
+    return pick(planet, [
+      "kepler_name",
+      "koi_prad",
+      "koi_smass",
+      "koi_srad",
+      "koi_count",
+      "koi_steff",
+    ]);
+  });
+}
+
+const newEarths = await loadPlanetData();
+for (const planet of newEarths) {
+  console.log(planet);
+}
+console.log(`${newEarths.length} habitable planets found!`);
